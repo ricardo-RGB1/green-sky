4ede2a7d81c222af55b8159f88d2fb732de4a296
Ricardo Garcia Bahena <garci283@cougars.csusm.edu>
2024-11-20 00:43:22 -0800

    update auth

--- middleware.ts
+++ middleware.ts
@@ -2,13 +2,16 @@ import { NextResponse } from "next/server";
 
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/"])
 
+// Create a route matcher for protected routes
+const isProtectedRoute = createRouteMatcher(["/"]) 
+
+// Clerk middleware to protect the route
 export default clerkMiddleware(async (auth, req) => {
-  if (isProtectedRoute(req)) {
-    await auth.protect()
+  if (isProtectedRoute(req)) { // If the route is protected
+    await auth.protect() // Protects the route
   }
-  return NextResponse.next()
+  return NextResponse.next() // If the route is not protected, return the next response
 });
 
 export const config = {
